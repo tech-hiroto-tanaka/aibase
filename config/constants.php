335cb4d319cda4772b1b7fdef2e2c9af62b87a83
htanaka <h.tanaka@areslot.jp>
2025-12-22 12:20:29 +0900

    二重定義解消

--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,9 @@
 <?php
 
-const PAGE_SIZE_LIMIT = [20, 50, 100];
-const PAGE_SIZE_DEFAULT = 20;
+if (!defined('PAGE_SIZE_LIMIT')) {
+    define('PAGE_SIZE_LIMIT', [20, 50, 100]);
+}
+
+if (!defined('PAGE_SIZE_DEFAULT')) {
+    define('PAGE_SIZE_DEFAULT', 20);
+}
